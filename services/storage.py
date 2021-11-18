df15bc7b7a586ac8aaa01974fc1b34c1d23cdc5d
Francisco Giudicatti <franciscogiudicatti11@gmail.com>
2021-11-17 22:24:17 -0300

    update17-1

--- services/storage.py
+++ services/storage.py
@@ -6,20 +6,25 @@ storage_folder = ""
 def historic_register(moneda,transaccion,logged_user,cantidad,price):
     archivo=open('transactions.txt','a+')
     now = datetime.now()
-    valor = float(cantidad)*float(price)
+    value = float(cantidad)*float(price)
     date = str(now.strftime("%d de %B de %Y"))
-    archivo.write(date+"|"+moneda+"|"+transaccion+"|"+ logged_user + "|" + str(cantidad) + "|" + str(valor) + "\n")
+    archivo.write(date+"|"+moneda+"|"+transaccion+"|"+ logged_user + "|" + str(cantidad) + "|" + str(value) + "\n")
     archivo.close()
 
-def mostrarHistorico():    
+def mostrarBalance():    
     print('*****                        DESPLEGAR HISTORICO                               *****')
-    archivo=open('historicos.txt','r')
+    archivo=open('balance.txt','r')
     texto = archivo.read()
     archivo.close()
     lineas = texto.splitlines()
     terminos = texto.split("|")
-    print("FECHA\t\t\t" + "MONEDA\t" + "TRANSACCION\t\t" + "USUARIO\t\t" + "CANTIDAD\tMONTO")
+    print("MONEDA\t\t\t" + "CANTIDAD\t")
     for linea in lineas:
         termino = linea.split("|")
-        print(termino[0] + "\t" + termino[1] + "\t" + termino[2]+ "\t\t" + termino[3] + "\t\t" + termino[4]+ "\t\t" + termino[4])
+        print(termino[0] + "\t" + termino[1] + "\t" + termino[2]+ "\t\t")
+
+def balance_register(moneda,logged_user,cantidad):
+    archivo=open('balance.txt','a+')
+    archivo.write(moneda+ "|" + logged_user + "|" + str(cantidad) + "|" + "\n")
+    archivo.close()
 
