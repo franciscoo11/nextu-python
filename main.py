d243e7bda2039407542a35fa12a03094bd0d69ed
Francisco Giudicatti <franciscogiudicatti11@gmail.com>
2021-11-17 22:03:29 -0300

    update17-11

--- main.py
+++ main.py
@@ -4,6 +4,17 @@ import requests #import del requests para utilizar get de las API's
 import os #import del OS para limpiar la terminal
 from datetime import datetime 
 
+def historic_register(moneda,transaccion,logged_user,cantidad,price):
+    archivo=open('transactions.txt','a+')
+    now = datetime.now()
+    valor = float(cantidad)*float(price)
+    date = str(now.strftime("%d de %B de %Y"))
+    archivo.write(date+"|"+moneda+"|"+transaccion+"|"+ logged_user + "|" + str(cantidad) + "|" + str(valor) + "\n")
+    archivo.close()
+
+
+
+
 history_file = "transactions.txt"
 global logged_user 
 
@@ -13,3 +24,4 @@ print("\n Bienvenido/a a tu billetera virtual de escritorio \n")
 # Por codigo (hardcodeado) o autenticacion
 logged_user = User(1)
 menu()
+
