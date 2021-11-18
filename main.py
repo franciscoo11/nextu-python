80dafd9cd4030b5c6ef38dd877e08aeea87cf9fe
Francisco Giudicatti <franciscogiudicatti11@gmail.com>
2021-11-17 22:18:16 -0300

    update17-11

--- main.py
+++ main.py
@@ -4,27 +4,6 @@ import requests #import del requests para utilizar get de las API's
 import os #import del OS para limpiar la terminal
 from datetime import datetime 
 
-def historic_register(moneda,transaccion,logged_user,cantidad,price):
-    archivo=open('transactions.txt','a+')
-    now = datetime.now()
-    valor = float(cantidad)*float(price)
-    date = str(now.strftime("%d de %B de %Y"))
-    archivo.write(date+"|"+moneda+"|"+transaccion+"|"+ logged_user + "|" + str(cantidad) + "|" + str(valor) + "\n")
-    archivo.close()
-
-def mostrarHistorico():    
-    print('******************************************************************************************************')
-    print('*****                        M O S T R A R   H I S T O R I C O S                                 *****')
-    print('******************************************************************************************************')
-    archivo=open('historicos.txt','r')
-    texto = archivo.read()
-    archivo.close()
-    lineas = texto.splitlines()
-    terminos = texto.split("|")
-    print("FECHA\t\t\t" + "MONEDA\t" + "TRANSACCION\t\t" + "USUARIO\t\t" + "CANTIDAD\tMONTO")
-    for linea in lineas:
-        termino = linea.split("|")
-        print(termino[0] + "\t" + termino[1] + "\t" + termino[2]+ "\t\t" + termino[3] + "\t\t" + termino[4]+ "\t\t" + termino[4])
 
 
 history_file = "transactions.txt"
--- services/storage.py
+++ services/storage.py
@@ -1,4 +1,25 @@
+from datetime import datetime
 transactions_file = "transactions.txt"
 balance_file = "balance.txt"
 storage_folder = ""
+
+def historic_register(moneda,transaccion,logged_user,cantidad,price):
+    archivo=open('transactions.txt','a+')
+    now = datetime.now()
+    valor = float(cantidad)*float(price)
+    date = str(now.strftime("%d de %B de %Y"))
+    archivo.write(date+"|"+moneda+"|"+transaccion+"|"+ logged_user + "|" + str(cantidad) + "|" + str(valor) + "\n")
+    archivo.close()
+
+def mostrarHistorico():    
+    print('*****                        DESPLEGAR HISTORICO                               *****')
+    archivo=open('historicos.txt','r')
+    texto = archivo.read()
+    archivo.close()
+    lineas = texto.splitlines()
+    terminos = texto.split("|")
+    print("FECHA\t\t\t" + "MONEDA\t" + "TRANSACCION\t\t" + "USUARIO\t\t" + "CANTIDAD\tMONTO")
+    for linea in lineas:
+        termino = linea.split("|")
+        print(termino[0] + "\t" + termino[1] + "\t" + termino[2]+ "\t\t" + termino[3] + "\t\t" + termino[4]+ "\t\t" + termino[4])
 
