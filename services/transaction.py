5a34b95b620a96b693a77e6b5ab765913b636665
Cristian Giudicatti <cristian@pryxsys.com>
2021-11-30 18:41:01 -0300

    update3

--- services/transaction.py
+++ services/transaction.py
@@ -6,9 +6,15 @@ def send(user_id,user_id_destine,amount,symbol):
     now = datetime.now()
     value = float(amount)*float(get_price(symbol))
     date = str(now.strftime("%d de %B de %Y"))
+
+    # Busco currency balance por simnbolo
+    # Actualizo el monto total de la moneda (cryptobalance.amount - amount)
+    # Llamamos a register_balance(user,cryptobalance) 
+
     register_transaction(date,user_id, symbol, amount, get_price(symbol))
     balance_register(symbol,user_id,amount)
     receive(user_id_destine, amount, symbol)
+    update_currencye_balance(symbol,user_id,amount)
     
 def receive(user_id, amount, symbol):
     now = datetime.now()
