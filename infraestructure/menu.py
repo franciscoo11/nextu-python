ee0135c66c37a6e5eeb70fa279272b8c79bd8c4e
Francisco Giudicatti <franciscogiudicatti11@gmail.com>
2021-12-03 22:18:38 -0300

    3/12/2021

--- infraestructure/menu.py
+++ infraestructure/menu.py
@@ -5,9 +5,9 @@ from operations.balance_general import show_general_balance
 from operations.balance_crypto import show_singular_balance
 import sys
 
-def menu(logged_user,show_menu):
+def menu(logged_user,back_to_menu):
     request_options = True
-    while show_menu == "S":
+    while back_to_menu == "S":
         print(f"""
         {menu_options.TRANSFER.value} \t\tTransferir dinero
         {menu_options.RECIVE.value} \t\tRecibir dinero
@@ -26,23 +26,27 @@ def menu(logged_user,show_menu):
                 
         if options == menu_options.TRANSFER.value:
             transfer(logged_user)
-            show_menu = input ("¿Desea volver al menú principal?, escriba S/N: ").upper()
-            menu(logged_user,show_menu)
+            back_to_menu = show_menu()
+            menu(logged_user,back_to_menu)
         elif options == menu_options.RECIVE.value:
             receive(logged_user)
-            show_menu = input ("¿Desea volver al menú principal?, escriba S/N: ").upper()
-            menu(logged_user,show_menu)
+            back_to_menu = show_menu()
+            menu(logged_user,back_to_menu)
         elif options == menu_options.CRYPTOCURRENCY_BALANCE.value:
             show_singular_balance(logged_user)
-            show_menu = input ("¿Desea volver al menú principal?, escriba S/N: ").upper()
-            menu(logged_user,show_menu)
+            back_to_menu = show_menu()
+            menu(logged_user,back_to_menu)
         elif options == menu_options.GENERAL_BALANCE.value:
             show_general_balance(logged_user)
-            show_menu = input ("¿Desea volver al menú principal?, escriba S/N: ").upper()
-            menu(logged_user,show_menu)
+            back_to_menu = show_menu()
+            menu(logged_user,back_to_menu)
         elif options == menu_options.HISTORYC_TRANSACTIONS.value:
             print("Funcion Historial de transacciones")
-            show_menu = input ("¿Desea volver al menú principal?, escriba S/N: ").upper()
-            menu(logged_user,show_menu)
+            back_to_menu = show_menu()
+            menu(logged_user,back_to_menu)
         else:
             sys.exit()
+
+
+def show_menu():
+    return input("¿Desea volver al menú principal?, escriba S/N: ").upper()
